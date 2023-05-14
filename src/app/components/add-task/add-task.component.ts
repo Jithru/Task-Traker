8aa8a8813be2dd8f9b8e7c79d2f22c27b282c36f
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-05-14 22:30:16 +0530

    done

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-add-task',
@@ -11,8 +13,16 @@ export class AddTaskComponent {
   text?: string;
   day?: string;
   reminder: boolean = false;
+
+  showAddTask?: boolean;
+  subscription?: Subscription;
+
+
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value)
+  }
   onSubmit() {
 
     if (!this.text) {
